ac83ec9d944a15253ee4c9f3e896e2c5b79c3c6d
låke <91898187+lutches@users.noreply.github.com>
2023-02-02 11:38:45 +0100

    Finished Views

--- Lab1/views.sql
+++ Lab1/views.sql
@@ -45,28 +45,81 @@ Create View UnreadMandatory AS (
 );
 
 Create View SumCredits AS (
-    Select student, Sum(credits) AS totalCredits
-    From FinishedCourses
-)
+    Select PassedCourses.student, Sum(credits) AS totalCredits
+    From PassedCourses
+    Group by passedCourses.student
+);
 
 Create View NumberOfUnread AS (
-Select student, Count(student) AS numberUnread
-From UnreadMandatory
-Group By student
+    Select student, Count(student) AS numberUnread
+    From UnreadMandatory
+    Group By student
 );
 
-Create View MathCredits
+Create View mathcredits AS (
+    Select Student, Sum(credits) AS mathcredits
+    From PassedCourses
+    Join Classified ON PassedCourses.course = Classified.course
+    where Classified.classifications = 'math'
+    Group by Student
+);
+
+Create View researchcredits AS (
+    Select Student, Sum(credits) AS researchcredits
+    From PassedCourses
+    Join Classified ON PassedCourses.course = Classified.course
+    where Classified.classifications = 'research'
+    Group by Student
+);
+
+Create View seminarcourses AS (
+    Select Student, Count(PassedCourses.course) AS seminarcourses
+    From PassedCourses
+    Join Classified ON PassedCourses.course = Classified.course
+    where Classified.classifications = 'seminar' 
+    Group by Student
+);
+
+Create View RecommendedCourses as (
+    Select PassedCourses.student, PassedCourses.course, PassedCourses.credits
+    From PassedCourses
+    LEFT JOIN basicinformation ON passedcourses.student=basicinformation.idnr
+    JOIN recommendedbranch
+    ON recommendedbranch.program=basicinformation.program
+    AND recommendedbranch.branch=basicinformation.branch
+    AND recommendedbranch.course=passedcourses.course
+
+);
+
+Create View recommendedCredits AS (
+    Select Student, Sum(RecommendedCourses.credits) AS total
+    From RecommendedCourses
+    Group By Student
+);
 
 Create View PathToGraduation AS (
-    Select FinishedCourses.student, FinishedCourses.credits AS totalCredits, NumberOfUnread.numberUnread AS mandatoryLeft, 'math' AS mathcredits, 'reasearch' AS researchCredits, 'seminarcourses' AS seminarcourses, True AS qualified
-    From FinishedCourses
-    outer join NumberOfUnread
-    ON FinishedCourses.student = NumberOfUnread.student
-    --outer join Classified
-    
+    Select 
+    BasicInformation.idnr AS student, 
+    Coalesce(totalCredits,0) AS totalcredits, 
+    Coalesce(numberUnread,0) AS mandatoryleft, 
+    Coalesce(mathcredits,0) AS mathcredits, 
+    Coalesce(researchcredits,0) AS researchcredits,
+    Coalesce(seminarcourses,0) AS seminarcourses,
+    basicinformation.branch IS NOT NULL
+    AND COALESCE(NumberOfUnread.numberUnread, 0) = 0
+    AND COALESCE(recommendedCredits.total, 0) >= 10
+    AND COALESCE(mathCredits.mathcredits, 0) >= 20
+    AND COALESCE(researchCredits.researchCredits, 0) >= 10
+    AND COALESCE(seminarCourses.seminarCourses, 0) > 0
+    AS qualified
 
-
-
-
-    -- Classified
+    FROM basicinformation
+    LEFT JOIN sumCredits ON idnr=sumCredits.student
+    LEFT JOIN NumberOfUnread ON idnr=NumberOfUnread.student
+    LEFT JOIN mathCredits ON idnr=mathCredits.student
+    LEFT JOIN researchCredits ON idnr=researchCredits.student
+    LEFT JOIN seminarCourses ON idnr=seminarCourses.student
+    LEFT JOIN recommendedCredits ON idnr=recommendedCredits.student
 );
+
+    
