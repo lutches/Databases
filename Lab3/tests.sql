578388bb64bd6560dc2988b734eb7384645368d1
låke <91898187+lutches@users.noreply.github.com>
2023-02-26 18:47:59 +0100

    lab 3 is correct according to autocheck

--- Lab3/tests.sql
+++ Lab3/tests.sql
@@ -1,37 +1,53 @@
---- TEST 1 : Register to an unlimited course
+
+
+--- TEST #1 : Register to an unlimited course
 --- EXP. OUTCOME : Pass
 INSERT INTO Registrations VALUES ('2222222222', 'CCC444');
-/*
---- TEST 2 : Register to a limited course
+
+--- TEST #2 : Register to a limited course
 --- EXP: OUTCOME : Pass
-INSERT INTO Registrations VALUES ('1111111111', 'AAA111')
+INSERT INTO Registrations VALUES ('6666666666', 'CCC333');
 
---- TEST 3 : Waiting for a limited course
---- EXP: OUTCOME : 
+--- TEST #3 : Waiting for a limited course
+--- EXP: OUTCOME : pass
+INSERT INTO Registrations VALUES ('3333333333','CCC333');
 
+--- TEST #4 : Remove from a waitinglist with additional students in the list 
+--- EXP. OUTCOME : pass
+DELETE FROM Registrations WHERE student = '2222222222' and course = 'CCC333';
+--- TEST #5 : Unregister from an unlimited course
+--- EXP. OUTCOME : Pass 
+DELETE FROM Registrations WHERE student = '2222222222' and course = 'CCC444';
 
---- TEST 4 : Remove from a waitinglist with additional students in the list 
---- EXP. OUTCOME : 
-
---- TEST 5 : Unregister from an unlimited course
+--- TEST #6 : Unregister from a limited course without a waiting list 
 --- EXP. OUTCOME : Pass
-DELETE FROM  Registrations WHERE student = '1111111111'  AND course = 'AAA111'
+DELETE FROM Registrations WHERE student = '2222222222' and course = 'CCC777';
 
---- TEST 6 : Unregister from a limited course without a waiting list 
+--- TEST #7 : Unregister from a limited course with a waiting list when the student is registered
+--- EXP. OUTCOME : Pass
+DELETE FROM Registrations WHERE student = '1111111111' and course = 'CCC333';
+
+--- TEST #8 : Unregister from a limited course with a waiting list when the student is in the middle of the waiting list
+--- EXP. OUTCOME : Pass
+DELETE FROM Registrations WHERE student = '3333333333' and course = 'CCC666';
+
+--- TEST #9 : Unregister from an overfull course with a waiting list
 --- EXP. OUTCOME : 
-INSERT INTO Registrations VALUES ()
+DELETE FROM Registrations WHERE student = '5555555555' and course = 'CCC666';
 
---- TEST 7 : Unregister from a limited course with a waiting list when the student is registered
---- EXP. OUTCOME : 
-INSERT INTO Registrations VALUES ()
+-- TEST #10: Register an already registered student.
+-- EXPECTED OUTCOME: Fail
+INSERT INTO Registrations VALUES ('1111111111', 'CCC111');
 
---- TEST 8 : Unregister from a limited course with a waiting list when the student is in the middle of the waiting list
---- EXP. OUTCOME : 
-INSERT INTO Registrations VALUES ()
+-- TEST #11: Try to register for course already passed.
+-- EXPECTED OUTCOME: Fail
+INSERT INTO Registrations VALUES ('4444444444', 'CCC222');
 
---- TEST 9 : Unregister from an overfull course with a waiting list
---- EXP. OUTCOME : 
-INSERT INTO Registrations VALUES ()
+-- TEST #12: Try to register for a course where the prerequisites haven't been taken.
+-- EXPECTED OUTCOME: Fail
+INSERT INTO Registrations VALUES ('7777777777', 'CCC111');
 
 
-*/
+
+
+
--- Lab3/triggers.sql
+++ Lab3/triggers.sql
@@ -5,24 +5,53 @@ CREATE OR REPLACE VIEW CourseQueuePositions AS (
 
 CREATE OR REPLACE FUNCTION on_register() RETURNS trigger AS $on_register$
      -- Check that a student is allowed to register for the course
-      -- [] Fullfills prerequisite courses
-      -- [] Not already registered for the course
-      -- [] Not on the waiting list
-      -- [] Not already passed the course
-
-      -- [] Check if the course is full and put on waiting list
+      -- [x] Fullfills prerequisite courses
+      -- [x] Not already registered for the course
+      -- [x] Not on the waiting list
+      -- [x] Not already passed the course
+      -- [x] Check if the course is full and put on waiting list
     Declare
         coursecapacity INT;
         coursecount INT;
-
+        coursestatus Text;
 
     BEGIN
-        IF (true) THEN
+    -- first test that always fails to see if the trigger works correctly 
+        /*IF (true) THEN
             RAISE EXCEPTION 'lmao du tror';
         END IF;
+        */
+    coursestatus := (Select status from registrations where student = NEW.student and course = NEW.course);
 
-     coursecapacity := (SELECT capacity FROM limitedcourses WHERE limitedcourses.code=NEW.course);
-      coursecount := (SELECT COUNT(*) FROM registrations WHERE course=NEW.course AND status='registered');
+        IF coursestatus = 'registered' THEN
+            RAISE EXCEPTION 'already registered for this course';
+        END IF;
+
+        IF coursestatus = 'waiting' THEN
+            RAISE EXCEPTION 'already waiting for this course';
+        END IF;
+
+        IF (
+        SELECT
+            COUNT(*)FROM 
+            prerequisites
+            LEFT JOIN passedcourses ON
+            passedcourses.student=NEW.student AND
+            passedcourses.course=prerequisite
+            WHERE prerequisites.course=NEW.course AND student IS NULL)>0
+            THEN
+        RAISE EXCEPTION 'Not eligible for course';
+      END IF;
+            
+        IF (select credits from passedCourses WHERE 
+        PassedCourses.course = NEW.Course and 
+        PassedCourses.student = NEW.student)>0 THEN
+            RAISE EXCEPTION 'has already passed this course';
+            END IF;
+
+
+        coursecapacity := (SELECT capacity FROM limitedcourses WHERE limitedcourses.code=NEW.course);
+        coursecount := (SELECT COUNT(*) FROM registrations WHERE course=NEW.course AND status='registered');
 
       IF coursecapacity IS NOT NULL AND coursecount >= coursecapacity THEN
         INSERT INTO waitinglist VALUES (NEW.student, NEW.course);
@@ -36,11 +65,35 @@ CREATE OR REPLACE FUNCTION on_register() RETURNS trigger AS $on_register$
 $on_register$ LANGUAGE plpgsql;
 
 
-Create OR REPLACE TRIGGER on_register INSTEAD OF INSERT OR UPDATE ON Registrations
+CREATE OR REPLACE FUNCTION on_unregister() RETURNS trigger AS $on_unregister$
+  DECLARE
+    coursecapacity INT;
+    coursecount INT;
+    firstinqueue VARCHAR(10);
+  BEGIN
+      DELETE FROM registered WHERE student=OLD.student AND course=OLD.course;
+      DELETE FROM waitinglist WHERE student=OLD.student AND course=OLD.course;
+
+      coursecapacity := (SELECT capacity FROM limitedcourses WHERE limitedcourses.code=OLD.course);
+      coursecount := (SELECT COUNT(*) FROM registrations WHERE course=OLD.course AND status='registered');
+
+      IF coursecapacity IS NOT NULL AND coursecount < coursecapacity THEN
+        firstinqueue := (SELECT student FROM coursequeuepositions WHERE place=1 AND course=OLD.course);
+
+        IF firstinqueue IS NOT NULL THEN
+          DELETE FROM waitinglist WHERE student=firstinqueue AND course=OLD.course;
+          INSERT INTO registered VALUES (firstinqueue, OLD.course);
+        END IF;
+      END IF;
+    RETURN OLD;
+END;
+$on_unregister$ LANGUAGE plpgsql;
+
+Create TRIGGER on_register INSTEAD OF INSERT OR UPDATE ON Registrations
     FOR EACH ROW EXECUTE FUNCTION on_register();
 
---Create OR REPLACE TRIGGER on_unregister INSTEAD OF DELETE ON Registrations
-  --  FOR EACH ROW EXECUTE FUNCTION on_unregister();
+Create TRIGGER on_unregister INSTEAD OF DELETE ON Registrations
+    FOR EACH ROW EXECUTE FUNCTION on_unregister();
 
 
 
