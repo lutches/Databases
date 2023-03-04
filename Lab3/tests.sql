c70a06477b1f8dc68e0bcdbec25b329f13c60073
låke <91898187+lutches@users.noreply.github.com>
2023-03-04 19:39:03 +0100

    lab 3

--- Lab3/tests.sql
+++ Lab3/tests.sql
@@ -30,25 +30,26 @@ DELETE FROM Registrations WHERE student = '1111111111' and course = 'CCC333';
 --- TEST #8 : Unregister from a limited course with a waiting list when the student is in the middle of the waiting list
 --- EXP. OUTCOME : Pass
 DELETE FROM Registrations WHERE student = '8888888888' and course = 'CCC888';
-/*
+
 --- TEST #9 : Unregister from an overfull course with a waiting list
 --- EXP. OUTCOME : 
-DELETE FROM Registrations WHERE student = '5555555555' and course = 'CCC666';
+DELETE FROM Registrations WHERE student = '9999999999' and course = 'CCC999';
 
 -- TEST #10: Register an already registered student.
 -- EXPECTED OUTCOME: Fail
-INSERT INTO Registrations VALUES ('1111111111', 'CCC111');
+INSERT INTO Registrations VALUES ('0000000010', 'CCC010');
 
 -- TEST #11: Try to register for course already passed.
 -- EXPECTED OUTCOME: Fail
-INSERT INTO Registrations VALUES ('4444444444', 'CCC222');
+INSERT INTO Registrations VALUES ('0000000011', 'CCC011');
 
 -- TEST #12: Try to register for a course where the prerequisites haven't been taken.
 -- EXPECTED OUTCOME: Fail
-INSERT INTO Registrations VALUES ('7777777777', 'CCC111');
+INSERT INTO Registrations VALUES ('0000000012', 'CCC012');
 
 
+-- TEST #13: Register to a course where prerequisites are fulfilled
+-- EXPECTED OUTCOME: Pass
+INSERT INTO Registrations VALUES ('0000000013', 'CCC013');
 
 
-
-*/
