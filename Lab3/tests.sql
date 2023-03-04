281fa7ecb51a9d558fbf3588803b6c38f88ba0fd
låke <91898187+lutches@users.noreply.github.com>
2023-03-04 18:19:28 +0100

    lab 3 tests becoming more manageble

--- Lab3/tests.sql
+++ Lab3/tests.sql
@@ -2,11 +2,11 @@
 
 --- TEST #1 : Register to an unlimited course
 --- EXP. OUTCOME : Pass
-INSERT INTO Registrations VALUES ('2222222222', 'CCC444');
+INSERT INTO Registrations VALUES ('1111111111', 'CCC111');
 
 --- TEST #2 : Register to a limited course
 --- EXP: OUTCOME : Pass
-INSERT INTO Registrations VALUES ('6666666666', 'CCC333');
+INSERT INTO Registrations VALUES ('2222222222', 'CCC222');
 
 --- TEST #3 : Waiting for a limited course
 --- EXP: OUTCOME : pass
@@ -14,14 +14,14 @@ INSERT INTO Registrations VALUES ('3333333333','CCC333');
 
 --- TEST #4 : Remove from a waitinglist with additional students in the list 
 --- EXP. OUTCOME : pass
-DELETE FROM Registrations WHERE student = '2222222222' and course = 'CCC333';
+DELETE FROM Registrations WHERE student = '4444444444' and course = 'CCC444';
 --- TEST #5 : Unregister from an unlimited course
 --- EXP. OUTCOME : Pass 
-DELETE FROM Registrations WHERE student = '2222222222' and course = 'CCC444';
+DELETE FROM Registrations WHERE student = '5555555555' and course = 'CCC555';
 
 --- TEST #6 : Unregister from a limited course without a waiting list 
 --- EXP. OUTCOME : Pass
-DELETE FROM Registrations WHERE student = '2222222222' and course = 'CCC777';
+DELETE FROM Registrations WHERE student = '6666666666' and course = 'CCC666';
 
 --- TEST #7 : Unregister from a limited course with a waiting list when the student is registered
 --- EXP. OUTCOME : Pass
@@ -29,8 +29,8 @@ DELETE FROM Registrations WHERE student = '1111111111' and course = 'CCC333';
 
 --- TEST #8 : Unregister from a limited course with a waiting list when the student is in the middle of the waiting list
 --- EXP. OUTCOME : Pass
-DELETE FROM Registrations WHERE student = '3333333333' and course = 'CCC666';
-
+DELETE FROM Registrations WHERE student = '8888888888' and course = 'CCC888';
+/*
 --- TEST #9 : Unregister from an overfull course with a waiting list
 --- EXP. OUTCOME : 
 DELETE FROM Registrations WHERE student = '5555555555' and course = 'CCC666';
@@ -51,3 +51,4 @@ INSERT INTO Registrations VALUES ('7777777777', 'CCC111');
 
 
 
+*/
---
+++ Lab4/PortalConnection.py
@@ -0,0 +1,50 @@
+import psycopg2
+
+
+class PortalConnection:
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            host="localhost",
+            user="postgres",
+            password="postgres")
+        self.conn.autocommit = True
+
+    def getInfo(self,student):
+      with self.conn.cursor() as cur:
+        # Here's a start of the code for this part
+        sql = """
+                SELECT jsonb_build_object(
+                     'student', s.idnr
+                    ,'name', s.name
+                ) :: TEXT
+                FROM BasicInformation AS s
+                WHERE s.idnr = %s;"""
+        cur.execute(sql, (student,))
+        res = cur.fetchone()
+        if res:
+            return (str(res[0]))
+        else:
+            return """{"student":"Not found :("}"""
+
+    def register(self, student, courseCode):
+        try:
+            #Your code goes here! Remove this comment and the line below it. 
+            return """{"success":false, "error":"Registration not implemented"}"""
+        except psycopg2.Error as e:
+            message = getError(e)
+            return '{"success":false, "error": "'+message+'"}'
+
+    def unregister(self, student, courseCode):
+        try:
+            #Your code goes here! Remove this comment and the line below it. 
+            return """{"success":false, "error":"Unregistration not implemented"}"""
+        except psycopg2.Error as e:
+            message = getError(e)
+            return '{"success":false, "error": "'+message+'"}'
+
+def getError(e):
+    message = repr(e)
+    message = message.replace("\\n"," ")
+    message = message.replace("\"","\\\"")
+    return message
+
